0aed68f869c519a570cf20b3472959fce0ac43a1
mazharulislam-12 <505mazharulislam@gmail.com>
2023-08-21 23:24:30 -0700

    array check and object check

---
+++ validation.js
@@ -0,0 +1,18 @@
+// string, number, boolean 
+const number = 23;
+if (typeof number === 'number') {
+    console.log('value is a number');
+}
+else{
+    console.log('value is not a number');
+}
+
+//  array check
+const numbers = [2,5,9,7,3];
+console.log(Array.isArray( numbers));
+
+// object check
+const student = {name: 'Rafsan', jon: 'students'}
+console.log(typeof student);
+
+
